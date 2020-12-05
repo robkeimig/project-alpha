6686328c4a962d4887fccbb1531b727d92d58904
Rob Keimig <rob@zenify.io>
2020-12-05 14:07:28 -0600

    Spike solution

---
+++ Alpha/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Alpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
